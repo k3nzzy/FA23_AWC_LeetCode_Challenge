d280f1cb301a22dd5f4da99e5ee8f018f8d01be6
cshen0 <69262309+cshen0@users.noreply.github.com>
2023-09-30 05:41:48 -0400

    week 3

---
+++ Caroline Shen (czs6068)/543.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        result = 0
+
+        def dfs(root):
+            nonlocal result
+            
+            if not root:
+                return -1
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            result  = max(result, 2 + left + right )
+
+            return 1 + max(left, right)
+        
+        dfs(root)
+        return result
